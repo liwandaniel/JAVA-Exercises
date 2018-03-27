3d545900497a849cb96c745984ffd79a96152256
System Administrator <root@bufu.local>
2018-03-27 16:05:32 +0800

    转换进制

--- decimalismtohexadecimal.java
+++ decimalismtohexadecimal.java
@@ -2,20 +2,39 @@
 class decimalismtohexadecimal
 {
 	public static void main(String[] args) {
-		toHex(60);
+		toHex(-60);
 	}
 
 	public static void toHex(int num)
-	{
-		StringBuffer StringBufferObject = new StringBuffer();    //字符串容器，存在内存中
-		for (int x=0;x<8;x++) {
+	{	
+		//查表转换 
+		char[] chs = {'0','1','2','3',
+						'4','5','6','7',
+						'8','9','A','B',
+						'C','D','E','F'};
+		//自定义一个容器
+		char[] arr = new char[8];
+		int pos = arr.length-1;
+		while(num!=0)
+		{
 			int temp = num & 15;
-			if(temp>9)
-				StringBufferObject.append((char)(temp-10+'A'));
-			else
-				StringBufferObject.append(temp);
-			num = num >>> 4;
+			arr[pos] = chs[temp];
+			pos--;
+			num = num >>>4;
 		}
-		System.out.println(StringBufferObject.reverse());
+		//遍历打印
+		for (int x=0; x<arr.length; x++) {
+			System.out.print(arr[x]);
+		}
+		// StringBuffer StringBufferObject = new StringBuffer();    //字符串容器，存在内存中
+		// for (int x=0;x<8;x++) {
+		// 	int temp = num & 15;
+		// 	if(temp>9)
+		// 		StringBufferObject.append((char)(temp-10+'A'));
+		// 	else
+		// 		StringBufferObject.append(temp);
+		// 	num = num >>> 4;
+		// }
+		// System.out.println(StringBufferObject.reverse());
 	} 
 }

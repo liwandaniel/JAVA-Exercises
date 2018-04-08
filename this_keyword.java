87151fc1a7b9e7d2374ae1badfc308e84ae498d7
System Administrator <root@bufu.local>
2018-04-08 17:33:33 +0800

    add this keyword demo

---
+++ this_keyword.java
@@ -0,0 +1,40 @@
+class Person
+{
+	private String name;
+	private String country = "cn";
+	public void show()
+	{
+		System.out.println(name+"::"+country);
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public String getName()
+	{
+		return this.name;
+	}
+
+	public void setCountry(String country)
+	{
+		this.country = country;
+	}
+
+	public String getCountry()
+	{
+		return this.country;
+	}
+}
+
+
+class thisDemo
+{
+	public static void main(String[] args) {
+		Person p = new Person();
+		p.setName("zhangsan");
+		System.out.println("result: " + p.getName());
+
+	}
+}

c7f21ed88e162f54cce0f510d2effa919fa4edcd
System Administrator <root@bufu.local>
2018-04-08 15:44:49 +0800

    constructor构造函数

---
+++ constructorDemo.java
@@ -0,0 +1,14 @@
+class constructor
+{
+	constructor()
+	{
+		System.out.println("run constructor");
+	}
+}
+
+class constructorDemo
+{
+	public static void main(String[] args) {
+		constructor c = new constructor();
+	}
+}
